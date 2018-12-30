7c966533caa237bac598fbabf67a2a3098d804a6
Yuan <yuanjiace@buaa.edu.cn>
2018-12-30 22:26:37 +0800

    modify Sampleland for upload

--- app/src/main/java/com/thcreate/vegsurveyassistant/db/entity/model/Sampleland.java
+++ app/src/main/java/com/thcreate/vegsurveyassistant/db/entity/model/Sampleland.java
@@ -20,27 +20,7 @@ public class Sampleland implements Parcelable {
     public String landId;//样地ID
     public String code;//样地编号
     public String type;//样地类型
-    @Expose
-    @SerializedName("administrative_region")
-    public String administrativeRegion;//行政区域
-    @Expose
-    @SerializedName("aspect")
-    public String aspect;//坡向
-    @Expose
-    @SerializedName("slope")
-    public String slope;//坡度
-    @Expose
-    @SerializedName("slope_position")
-    public String slopePosition;//坡位
-    @Expose
-    @SerializedName("landform")
-    public String landform;//地貌
-    @Expose
-    @SerializedName("soil_characteristic")
-    public String soilCharacteristic;//土壤特征
-    @Expose
-    @SerializedName("human_activity")
-    public String humanActivity;//人类活动
+    public LandData data;//数据
     public String lng;//经度
     public String lat;//纬度
     public String alt;//海拔
@@ -50,12 +30,14 @@ public class Sampleland implements Parcelable {
     public Date deleteAt;//删除时间
 
     public Sampleland() {
+        this.data = new LandData();
     }
 
     public Sampleland(int userId, @NonNull String landId, @NonNull String type){
         this.userId = userId;
         this.landId = landId;
         this.type = type;
+        this.data = new LandData();
     }
 
     public Sampleland(int id, int userId, @NonNull String landId, @NonNull String type){
@@ -63,6 +45,7 @@ public class Sampleland implements Parcelable {
         this.userId = userId;
         this.landId = landId;
         this.type = type;
+        this.data = new LandData();
     }
 
     @Override
@@ -72,21 +55,15 @@ public class Sampleland implements Parcelable {
 
     @Override
     public void writeToParcel(Parcel dest, int flags) {
-        dest.writeValue(id);
-        dest.writeValue(userId);
-        dest.writeValue(landId);
-        dest.writeValue(code);
-        dest.writeValue(type);
-        dest.writeValue(administrativeRegion);
-        dest.writeValue(aspect);
-        dest.writeValue(slope);
-        dest.writeValue(slopePosition);
-        dest.writeValue(landform);
-        dest.writeValue(soilCharacteristic);
-        dest.writeValue(humanActivity);
-        dest.writeValue(lng);
-        dest.writeValue(lat);
-        dest.writeValue(alt);
+        dest.writeInt(id);
+        dest.writeInt(userId);
+        dest.writeString(landId);
+        dest.writeString(code);
+        dest.writeString(type);
+        dest.writeParcelable(data, PARCELABLE_WRITE_RETURN_VALUE);
+        dest.writeString(lng);
+        dest.writeString(lat);
+        dest.writeString(alt);
         if (createAt == null){
             dest.writeValue(null);
         }
@@ -126,66 +103,75 @@ public class Sampleland implements Parcelable {
     };
 
     public Sampleland(Parcel source){
-        Object tmpId = source.readValue(getClass().getClassLoader());
-        if (tmpId != null){
-            id = (int)tmpId;
-        }
-        Object tmpUserId = source.readValue(getClass().getClassLoader());
-        if (tmpUserId != null){
-            userId = (int)tmpUserId;
-        }
-        Object tmpLandId = source.readValue(getClass().getClassLoader());
-        if (tmpLandId != null){
-            landId = (String)tmpLandId;
-        }
-        Object tmpCode = source.readValue(getClass().getClassLoader());
-        if (tmpCode != null){
-            code = (String)tmpCode;
-        }
-        Object tmpType = source.readValue(getClass().getClassLoader());
-        if (tmpType != null){
-            type = (String)tmpType;
-        }
-        Object tmpAdministrativeRegion = source.readValue(getClass().getClassLoader());
-        if (tmpAdministrativeRegion != null){
-            administrativeRegion = (String)tmpAdministrativeRegion;
-        }
-        Object tmpAspect = source.readValue(getClass().getClassLoader());
-        if (tmpAspect != null){
-            aspect = (String)tmpAspect;
-        }
-        Object tmpSlope = source.readValue(getClass().getClassLoader());
-        if (tmpSlope != null){
-            slope = (String)tmpSlope;
-        }
-        Object tmpSlopePosition = source.readValue(getClass().getClassLoader());
-        if (tmpSlopePosition != null){
-            slopePosition = (String)tmpSlopePosition;
-        }
-        Object tmpLandform = source.readValue(getClass().getClassLoader());
-        if (tmpLandform != null){
-            landform = (String)tmpLandform;
-        }
-        Object tmpSoilCharacteristic = source.readValue(getClass().getClassLoader());
-        if (tmpSoilCharacteristic != null){
-            soilCharacteristic = (String)tmpSoilCharacteristic;
-        }
-        Object tmpHumanActivity = source.readValue(getClass().getClassLoader());
-        if (tmpHumanActivity != null){
-            humanActivity = (String)tmpHumanActivity;
-        }
-        Object tmpLng = source.readValue(getClass().getClassLoader());
-        if (tmpLng != null){
-            lng = (String)tmpLng;
-        }
-        Object tmpLat = source.readValue(getClass().getClassLoader());
-        if (tmpLat != null){
-            lat = (String)tmpLat;
-        }
-        Object tmpAlt = source.readValue(getClass().getClassLoader());
-        if (tmpAlt != null){
-            alt = (String)tmpAlt;
-        }
+        id = source.readInt();
+        userId = source.readInt();
+        landId = source.readString();
+        code = source.readString();
+        type = source.readString();
+        data = source.readParcelable(LandData.class.getClassLoader());
+        lng = source.readString();
+        lat = source.readString();
+        alt = source.readString();
+//        Object tmpId = source.readValue(getClass().getClassLoader());
+//        if (tmpId != null){
+//            id = (int)tmpId;
+//        }
+//        Object tmpUserId = source.readValue(getClass().getClassLoader());
+//        if (tmpUserId != null){
+//            userId = (int)tmpUserId;
+//        }
+//        Object tmpLandId = source.readValue(getClass().getClassLoader());
+//        if (tmpLandId != null){
+//            landId = (String)tmpLandId;
+//        }
+//        Object tmpCode = source.readValue(getClass().getClassLoader());
+//        if (tmpCode != null){
+//            code = (String)tmpCode;
+//        }
+//        Object tmpType = source.readValue(getClass().getClassLoader());
+//        if (tmpType != null){
+//            type = (String)tmpType;
+//        }
+//        Object tmpAdministrativeRegion = source.readValue(getClass().getClassLoader());
+//        if (tmpAdministrativeRegion != null){
+//            administrativeRegion = (String)tmpAdministrativeRegion;
+//        }
+//        Object tmpAspect = source.readValue(getClass().getClassLoader());
+//        if (tmpAspect != null){
+//            aspect = (String)tmpAspect;
+//        }
+//        Object tmpSlope = source.readValue(getClass().getClassLoader());
+//        if (tmpSlope != null){
+//            slope = (String)tmpSlope;
+//        }
+//        Object tmpSlopePosition = source.readValue(getClass().getClassLoader());
+//        if (tmpSlopePosition != null){
+//            slopePosition = (String)tmpSlopePosition;
+//        }
+//        Object tmpLandform = source.readValue(getClass().getClassLoader());
+//        if (tmpLandform != null){
+//            landform = (String)tmpLandform;
+//        }
+//        Object tmpSoilCharacteristic = source.readValue(getClass().getClassLoader());
+//        if (tmpSoilCharacteristic != null){
+//            soilCharacteristic = (String)tmpSoilCharacteristic;
+//        }
+//        Object tmpHumanActivity = source.readValue(getClass().getClassLoader());
+//        if (tmpHumanActivity != null){
+//            humanActivity = (String)tmpHumanActivity;
+//        }
+//        Object tmpLng = source.readValue(getClass().getClassLoader());
+//        if (tmpLng != null){
+//            lng = (String)tmpLng;
+//        }
+//        Object tmpLat = source.readValue(getClass().getClassLoader());
+//        if (tmpLat != null){
+//            lat = (String)tmpLat;
+//        }
+//        Object tmpAlt = source.readValue(getClass().getClassLoader());
+//        if (tmpAlt != null){
+//            alt = (String)tmpAlt;
+//        }
         Object tmpCreateAt = source.readValue(getClass().getClassLoader());
         if (tmpCreateAt != null){
             createAt = new Date((Long)tmpCreateAt);
@@ -205,18 +191,18 @@ public class Sampleland implements Parcelable {
     }
 
     public static Sampleland getInstance(SamplelandEntity entity){
-        Sampleland data;
+        Sampleland data = new Sampleland();
+        data.initCommonFromEntity(entity);
         if (entity.data != null){
             Gson gson = new GsonBuilder()
                     .excludeFieldsWithoutExposeAnnotation()
                     .serializeNulls()
                     .create();
-            data = gson.fromJson(entity.data, Sampleland.class);
+            data.data = gson.fromJson(entity.data, LandData.class);
         }
-        else {
-            data = new Sampleland();
-        }
-        data.initCommonFromEntity(entity);
+//        else {
+//            data = new Sampleland();
+//        }
         return data;
     }
 
@@ -242,8 +228,75 @@ public class Sampleland implements Parcelable {
                 .excludeFieldsWithoutExposeAnnotation()
                 .serializeNulls()
                 .create();
-        data.data = gson.toJson(this, Sampleland.class);
+        data.data = gson.toJson(this.data, LandData.class);
         return data;
+    }
+
+    public static class LandData implements Parcelable {
+
+        public LandData() {
+        }
+
+        @Expose
+        @SerializedName("administrative_region")
+        public String administrativeRegion;//行政区域
+        @Expose
+        @SerializedName("aspect")
+        public String aspect;//坡向
+        @Expose
+        @SerializedName("slope")
+        public String slope;//坡度
+        @Expose
+        @SerializedName("slope_position")
+        public String slopePosition;//坡位
+        @Expose
+        @SerializedName("landform")
+        public String landform;//地貌
+        @Expose
+        @SerializedName("soil_characteristic")
+        public String soilCharacteristic;//土壤特征
+        @Expose
+        @SerializedName("human_activity")
+        public String humanActivity;//人类活动
+
+        @Override
+        public int describeContents() {
+            return 0;
+        }
+
+        @Override
+        public void writeToParcel(Parcel parcel, int i) {
+            parcel.writeString(administrativeRegion);
+            parcel.writeString(aspect);
+            parcel.writeString(slope);
+            parcel.writeString(slopePosition);
+            parcel.writeString(landform);
+            parcel.writeString(soilCharacteristic);
+            parcel.writeString(humanActivity);
+        }
+
+        public static final Parcelable.Creator<LandData> CREATOR = new Creator<LandData>() {
+            @Override
+            public LandData createFromParcel(Parcel source) {
+                return new LandData(source);
+            }
+
+            @Override
+            public LandData[] newArray(int size) {
+                return new LandData[0];
+            }
+        };
+
+        public LandData(Parcel source){
+            administrativeRegion = source.readString();
+            aspect = source.readString();
+            slope = source.readString();
+            slopePosition = source.readString();
+            landform = source.readString();
+            soilCharacteristic = source.readString();
+            humanActivity = source.readString();
+        }
+
     }
 
 }
